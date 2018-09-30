f7f1c250693d5e5fa10f6c5acf44ef989837c37a
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-30 16:09:16 +0300

    succesfully log in to and access workout

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const rp = require('request-promise')
 const cheerio = require('cheerio')
 
 const options = {
-  uri: 'url to scrape',
+  uri: 'http://www.yancycamp.com/allison-tai/yancy-camp-workout-468/',
   transform: function (body) {
     return cheerio.load(body)
-  }
+  },
+  headers: {'content-type': 'application/x-www-form-urlencoded'},
+  body: 'login_user_name=YOURUSERNAME&login_pwd=aYOURPASSWORD&Submit=doLogin'
 }
 
-rp(options)
-  .then(data => {
-  // do something
+rp.post(options)
+  .then(($) => {
+    let x = $('p').text()
+    console.log(x)
   })
   .catch(err => {
     console.error(err)
